8f1092f49dd75a0ebf058d0ee579f8c6a5997271
Warrioravi <abhisinghraj2k18@gmail.com>
2021-05-25 15:01:11 +0530

    deploy to heroku

--- src/mYCOMPONENTS/Header.js
+++ src/mYCOMPONENTS/Header.js
@@ -24,7 +24,7 @@ export default function Header(props) {
          props.searchBar ? <form className="d-flex">
          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
          <button className="btn btn-outline-success" type="submit">Search</button>
-       </form> :""
+       </form> :"   "
       }
     </div>
   </div>
