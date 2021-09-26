c8884f934b9819a11bf281d1a848717a4391e815
Abhishek Kumar <36950025+Warrioravi@users.noreply.github.com>
2021-09-26 10:23:31 +0530

    Update Todos.js

--- src/mYCOMPONENTS/Todos.js
+++ src/mYCOMPONENTS/Todos.js
@@ -4,7 +4,7 @@ import { TodoItem } from './TodoItem'
 export const Todos = (props) => {//named export
     return (
         <div className="container">
-           <h3>Inside todos</h3>
+             <h3>TODOLIST</h3>
            {props.todos.length===0? "No todos to Display" :
             props.todos.map((todo) =>{
             return <TodoItem todo={todo} key={todo.serial} onDelete={props.onDelete}/>
